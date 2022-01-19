aff87c63757c0e42171ee0c13cb4e9694b75366b
Guilherme Ferreira <alefff.guilherme@gmail.com>
2022-01-19 13:45:43 -0300

    Save score

--- backend/src/main/java/com/guilherme/dsmovie/repositories/ScoreRepository.java
+++ backend/src/main/java/com/guilherme/dsmovie/repositories/ScoreRepository.java
@@ -3,9 +3,9 @@ package com.guilherme.dsmovie.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.guilherme.dsmovie.entities.Movie;
-
+import com.guilherme.dsmovie.entities.Score;
+import com.guilherme.dsmovie.entities.ScorePK;
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {
+public interface ScoreRepository extends JpaRepository<Score, ScorePK> {
 
 }
--- backend/src/main/java/com/guilherme/dsmovie/repositories/UserRepository.java
+++ backend/src/main/java/com/guilherme/dsmovie/repositories/UserRepository.java
@@ -3,9 +3,10 @@ package com.guilherme.dsmovie.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.guilherme.dsmovie.entities.Movie;
-
+import com.guilherme.dsmovie.entities.User;
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByEmail(String email);
 
 }
